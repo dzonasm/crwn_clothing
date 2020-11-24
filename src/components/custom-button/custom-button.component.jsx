f38b702484b99bcc7015b80a2235032248b1f016
dzonasm <jmarciulevicius@gmail.com>
2020-11-24 11:26:38 +0200

    added a cart item component in the checkout page

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -5,7 +5,9 @@ import './custom-button.styles.scss'
 const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
     <button className={`
     ${inverted ? 'inverted' : ''}
-    ${isGoogleSignIn ? 'google-sign-in ' : ''}custom-button`} {...otherProps}>
+    ${isGoogleSignIn ? 'google-sign-in ' : ''}
+    custom-button`} 
+    {...otherProps}>
         {children}
     </button>
 
