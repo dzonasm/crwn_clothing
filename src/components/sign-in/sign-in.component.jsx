951e5ca33ff585d964acedb8213a3e9114083871
dzonasm <jmarciulevicius@gmail.com>
2020-12-14 20:12:01 +0200

    added useState and useEffect hooks in app, sign in, sign up and shop component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
 
 import "./sign-in.styles.scss"
@@ -8,70 +8,59 @@ import CustomButton from "../custom-button/custom-button.component"
 
 import { googleSignInStart, emailSignInStart } from '../../redux/user/user.actions'
 
-class SignIn extends React.Component {
-    constructor(props) {
-        super(props)
+const SignIn = ({ emailSignInStart, googleSignInStart }) => {
 
-        this.state = {
-            email: "",
-            password: ""
-        }
-    }
+    const [userCredentials, setCredentials] = useState({ email: '', password: '' })
 
-    handleSubmit = async (e) => {
+    const { email, password } = userCredentials
+
+    const handleSubmit = async (e) => {
         e.preventDefault()
-        const { emailSignInStart } = this.props
-        const { email, password } = this.state
-
         emailSignInStart(email, password)
     }
 
-    handleChange = (e) => {
+    const handleChange = (e) => {
         const { value, name } = e.target
 
-        this.setState({ [name]: value })
+        setCredentials({ ...userCredentials, [name]: value })
     }
 
 
-    render() {
+    return (
+        <div className="sign-in">
+            <h2>I already have an account</h2>
+            <span>Sign in with your email and password.</span>
 
-        const { googleSignInStart } = this.props
-        return (
-            <div className="sign-in">
-                <h2>I already have an account</h2>
-                <span>Sign in with your email and password.</span>
+            <form onSubmit={handleSubmit}>
+                <FormInput
+                    name="email"
+                    type="email"
+                    value={email}
+                    handleChange={handleChange}
+                    label="email"
+                    required
+                />
 
-                <form onSubmit={this.handleSubmit}>
-                    <FormInput
-                        name="email"
-                        type="email"
-                        value={this.state.email}
-                        handleChange={this.handleChange}
-                        label="email"
-                        required
-                    />
+                <FormInput
+                    name="password"
+                    type="password"
+                    value={password}
+                    handleChange={handleChange}
+                    label="password"
+                    required />
 
-                    <FormInput
-                        name="password"
-                        type="password"
-                        value={this.state.password}
-                        handleChange={this.handleChange}
-                        label="password"
-                        required />
-
-                    <div className="buttons">
-                        <CustomButton type='submit'>Sign In</CustomButton>
-                        <CustomButton
-                            type='button'
-                            onClick={googleSignInStart}
-                            isGoogleSignIn
-                        >
-                            Sign In with Google</CustomButton>
-                    </div>
-                </form>
-            </div>
-        )
-    }
+                <div className="buttons">
+                    <CustomButton type='submit'>Sign In</CustomButton>
+                    <CustomButton
+                        type='button'
+                        onClick={googleSignInStart}
+                        isGoogleSignIn
+                    >
+                        Sign In with Google</CustomButton>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 const mapDispatchToProps = dispatch => ({
