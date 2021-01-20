30cd85b9a9b61ca02a1f7ce13a1fe8d2ff2973e6
dzonasm <jmarciulevicius@gmail.com>
2021-01-20 20:10:20 +0200

    updated readme

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,7 @@ import React, { useEffect } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
-
-import './App.css'
+import { GlobalStyle } from './global.styles'
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component'
@@ -22,6 +21,7 @@ const App = ({ currentUser, checkUserSession }) => {
 
   return (
     <div>
+      <GlobalStyle />
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
